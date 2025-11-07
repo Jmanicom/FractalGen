252bec394924308667463763280e8a90c7a6b1c2
ymir <joshmanicom@gmail.com>
2025-11-06 19:55:02 -0800

    Created ev namespace/header file

---
+++ src/util/event_handler.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+namespace ev {                                  // Create namespace for event handling
+
+    void processEvents(sf::Window& window)      // Take in reference to window object from main.cpp
+    {
+        while (const std::optional event = window.pollEvent()) {
+                if (event->is<sf::Event::Closed>()) {
+                    window.close();
+                    break;
+                } else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+                    if (keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
+                        window.close();
+                        break;
+                }   if (keyPressed->scancode == sf::Keyboard::Scancode::R) {
+                        window.close(); // test for event handling
+                        break;
+                }
+            }
+        }
+    }
+}
