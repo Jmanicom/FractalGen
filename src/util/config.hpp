c2acfc7878bc5187ccf7aa8c2cf2d11f02dd2e76
ymir <joshmanicom@gmail.com>
2025-11-06 19:55:33 -0800

    Created config header for sim settings

---
+++ src/util/config.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+namespace conf
+{
+
+// Window Config
+sf::Vector2u const window_size = {1920, 1080};
+sf::Vector2f const window_size_f = static_cast<sf::Vector2f>(window_size);
+
+uint32_t const max_framerate = 144;
+int const max_iter = 200;
+}
