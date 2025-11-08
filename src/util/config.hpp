7565e17c7aaf18388c15de8e1fb52ba7e5a71cef
ymir <joshmanicom@gmail.com>
2025-11-08 00:51:26 -0800

    Updated maxIter

--- src/util/config.hpp
+++ src/util/config.hpp
@@ -11,7 +11,7 @@ sf::Vector2u const window_size = {1920, 1080};
 sf::Vector2f const window_size_f = static_cast<sf::Vector2f>(window_size);
 
 uint32_t const max_framerate = 144;
-int const max_iter = 200;
+int const max_iter = 500;
 bool const cursor_visible = false;
 bool const key_reapeat = false;
 
