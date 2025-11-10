8a581552bbc8e407561798c0b5515da96222e7bd
ymir <joshmanicom@gmail.com>
2025-11-10 03:51:06 -0800

    Modified zoom and iterations

--- src/util/config.hpp
+++ src/util/config.hpp
@@ -11,13 +11,13 @@ sf::Vector2u const window_size = {1920, 1080};
 sf::Vector2f const window_size_f = static_cast<sf::Vector2f>(window_size);
 
 uint32_t const max_framerate = 144;
-int const max_iter = 2500;
+int const max_iter = 2000;
 bool const cursor_visible = false;
 bool const key_reapeat = false;
 
 // Initial fractal settings (defaults)
 const sf::Vector2f center_init = {0.0f, 0.0f};
-const float zoom_init = 1.5f;
+const float zoom_init = 1.0f;
 
 
 // Control Settings (to be implemented)
