801c825e00aec3996eb2064e2c4759348b1282f3
ymir <joshmanicom@gmail.com>
2025-11-11 16:34:06 -0800

    Modified iteration count

--- src/util/config.hpp
+++ src/util/config.hpp
@@ -11,7 +11,7 @@ sf::Vector2u const window_size = {1920, 1080};
 sf::Vector2f const window_size_f = static_cast<sf::Vector2f>(window_size);
 
 uint32_t const max_framerate = 144;
-int const max_iter = 2000;
+int const max_iter = 2500;
 bool const cursor_visible = false;
 bool const key_reapeat = false;
 
